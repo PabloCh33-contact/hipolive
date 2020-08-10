8c8e4caabbd53065e7ed4e478f9480b17ed6353e
emilien44 <69353200+emilien44@users.noreply.github.com>
2020-08-10 18:14:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+	
+//responsive
+		var MenuItems = document.getElementById("MenuItems");
+
+		MenuItems.style.maxHeight = "0px";
+
+		function menutoggle(){
+			if(MenuItems.style.maxHeight =="0px")
+				{
+					MenuItems.style.maxHeight = "200px"
+				}
+			else
+				{
+					MenuItems.style.maxHeight = "0px"
+				}	
+			}
+//Star avis
+			const allStars = document.querySelectorAll(".fa-star");
+			console.log("allStars", allStars);
+
+			init();
+
+			function init(){
+				allStars.forEach(star => {
+					star.addEventListener("click", getRating);
+					star.addEventListener("mouseover", addCSS);
+					star.addEventListener("mouseleave", removeCSS);
+				});
+				}
+
+				function getRating(e) {
+					console.log(e.target.dataset, e.target.nodeName, e.target.nodeType);
+				}
+
+				function addCSS(e, css = "checked") {
+					const overedStar = e.target;
+					overedStar.classList.add(css);
+					const previousSiblings = getPreviousSiblings(overedStar);
+					console.log("previousSiblings", previousSiblings);
+				}
+				function removeCSS(e, css = "checked"){
+					e.target.classList.remove(css);
+
+				}
+
+				function getPreviousSiblings(elem) {
+					console.log("elem.previousSiblings", elem.previousSiblings);
+					let siblings = [];
+					const spanNodeType = 1;
+					while ((elem = elem.previousSiblings)) {
+						if (elem.nodeType === spanNodeType) {
+							siblings = [elem, ...siblings];
+						}
+
+					}
+						return siblings;
+
+					}
